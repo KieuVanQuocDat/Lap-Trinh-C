3920a71ed275edf684e1d24f642e8d65aea10ba8
KieuVanQuocDat <datkieuvan81@gmail.com>
2022-03-12 19:29:20 +0700

    commit

---
+++ BaiThucHanh2/BAI2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+	
+	{
+		int sum,n,c;
+		printf (" Nhap n = ");
+		scanf ("%d", &n);
+		while (c<=n)
+			{
+				
+				sum+=c;
+				c++;
+				
+			}
+		printf("Tong cua day so la : %d", sum);	
+	}
+	
+
