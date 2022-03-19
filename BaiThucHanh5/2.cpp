c3046e104cec61e0b5c585353bf4261182d11c12
KieuVanQuocDat <datkieuvan81@gmail.com>
2022-03-19 08:56:10 +0700

    commit

---
+++ BaiThucHanh5/2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf ("write a function to calculate area of a triangle. Hint : formula for triangle area : S= bottom_edge*height/2 or S = can bac 2 cua (p*(p-a)*(p-b)*(p-c) with p=(a+b+c)\2. a,c,b are 3 slides off triangle)");
+printf ("the function should return -1 for invalid triangle");
+	return 0 ;
+}
+
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+Push code
 1. Tao folder code
 2. Code, bo vao folder da tao
 3. Click phai vào folder LapTrinh -> Git Bash Here
@@ -5,3 +6,8 @@
   - git add .
   - git commit -m "commit"
   - git push
+
+Lay code ve:
+1. Go:
+  - git fetch
+  - git pull
